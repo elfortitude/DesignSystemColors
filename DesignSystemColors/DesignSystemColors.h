733786466132294e2395e68646366fc386f4ef4f
Усачева Елизавета Игоревна [B] <EIgUsacheva@sberbank.ru>
2022-08-14 17:22:15 +0300

    Initial commit

---
+++ DesignSystemColors/DesignSystemColors.h
@@ -0,0 +1,18 @@
+//
+//  DesignSystemColors.h
+//  DesignSystemColors
+//
+//  Created by Elizaveta Usacheva on 14.08.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DesignSystemColors.
+FOUNDATION_EXPORT double DesignSystemColorsVersionNumber;
+
+//! Project version string for DesignSystemColors.
+FOUNDATION_EXPORT const unsigned char DesignSystemColorsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DesignSystemColors/PublicHeader.h>
+
+
